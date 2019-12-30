6048ed92469cec51d707b35e8d1ce1a303aafb6d
Stephen Healey <s_healey@hotmail.co.uk>
2019-12-30 17:00:19 +0000

    Added list to notifications

---
+++ src/app/models/notificationEnum.enum.ts
@@ -0,0 +1,6 @@
+export enum NotificationEnum {
+    Debug = 0,
+    Info = 1,
+    Warning = 2,
+    Error = 3
+}
--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,23 +1,67 @@
 import { Injectable } from '@angular/core';
+import { NotificationEnum } from '../models/notificationEnum.enum';
+import { delay } from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  notificationMessage: string = 'Test Notification Service';
+  notificationMessage: string;
+  private notificationList = [] as Array<string>;
+  private setTimeoutId: any;
+  private notificationStillVisible = false;
+  private userHasAcknowledgeNotification = false;
 
 constructor() { }
 
-setNotification(message: string): void {
-  this.notificationMessage = message;
-  setTimeout(() => {
-    this.notificationMessage = null;
-  }, 3000);
+async setNotification(notificationLevel: NotificationEnum, message: string): Promise<void> {
+  const NEXT_MESSAGE = `${NotificationEnum[notificationLevel]} - ${message}`;
+  console.log('1');
+  if (this.notificationList.indexOf(NEXT_MESSAGE) === -1) {
+    console.log('2');
+    this.notificationList.push(NEXT_MESSAGE);
+    if (this.notificationMessage) {
+      console.log('3');
+      this.clearNotification();
+    }
+    while (this.notificationList.length > 0) {
+      await delay(100);
+      if (!this.notificationMessage) {
+        this.notificationMessage = this.notificationList[0];
+        this.notificationStillVisible = true;
+        this.setTimeoutId = setTimeout(() => {
+          this.clearNotification();
+        }, 6000);
+      }
+    }
+  }
 }
 
 clearNotification(): void {
-  this.notificationMessage = null;
+  if (!this.userHasAcknowledgeNotification) {
+    this.userHasAcknowledgeNotification = true;
+    clearTimeout(this.setTimeoutId);
+    this.setTimeoutId = null;
+
+    const ELEMENT = document.getElementById('notificationDiv') as HTMLDivElement;
+    if (ELEMENT) {
+      ELEMENT.animate([
+        // keyframes
+        { transform: 'translateY(0)' },
+        { transform: 'translateY(150px)' }
+      ], {
+        // timing options
+        duration: 2000
+      });
+    }
+
+    setTimeout(() => {
+      this.notificationMessage = null;
+      this.notificationList.splice(0, 1);
+      this.userHasAcknowledgeNotification = false;
+    }, 1800);
+  }
 }
 
 }
